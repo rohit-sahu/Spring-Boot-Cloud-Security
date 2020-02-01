25f0887e7523c806f1f88ea89c70de1c380fcb89
rohit-sahu <rohit.kumar@kuliza.com>
2020-02-01 14:27:30 +0530

    Added Authorization server

---
+++ security-oauth2-server/src/main/java/com/springboot/securityoauth2server/SecurityOauth2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.securityoauth2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOauth2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityOauth2ServerApplication.class, args);
+	}
+
+}
---
+++ security-oauth2-server/src/test/java/com/springboot/securityoauth2server/SecurityOauth2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.securityoauth2server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOauth2ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
